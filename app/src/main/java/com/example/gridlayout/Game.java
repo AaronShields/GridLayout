d1224ee83669b69f2a815ea5b78195a0b65bd47f
Aaron Shields <amshield@usc.edu>
2023-09-14 21:10:52 -0700

    Game.java updated

--- app/src/main/java/com/example/gridlayout/Game.java
+++ app/src/main/java/com/example/gridlayout/Game.java
@@ -1,5 +1,6 @@
 package com.example.gridlayout;
 
+import java.util.Random;
 
 public class Game {
 
@@ -7,16 +8,47 @@ public class Game {
     public static final int COLUMN_COUNT = 10;
     public static final int MINE_COUNT = 4;
 
+    private boolean[][] cellRevealed;
+    private int[][] gameBoard;
+    private boolean[][] mineLocations;
+
     private boolean gameWon;
     private boolean gameLost;
 
-    // Define your game logic here
-    // You can include methods and fields related to your game
 
     public Game() {
-        // Initialize your game state here
-    }
 
-    // Add methods and fields specific to your game
+        createGameboard();
+        placeMines();
+    }
+    private void createGameboard() {
+        //Created gameboard to match row and column count
+        gameBoard = new int[ROW_COUNT][COLUMN_COUNT];
+        for (int i = 0; i < ROW_COUNT; i++) {
+            for (int j = 0; j < COLUMN_COUNT; j++) {
+                gameBoard[i][j] = 0;
+            }
+        }
+    }
+    private void placeMines(){
+        //Random placed mines on the game board
+        mineLocations = new boolean[ROW_COUNT][COLUMN_COUNT];
+        Random random = new Random();
+        int minesPlaced = 0;
+
+
+        while (minesPlaced < MINE_COUNT){
+            int row = random.nextInt(ROW_COUNT);
+            int column = random.nextInt(COLUMN_COUNT);
+
+            if(!mineLocations[row][column]){
+                mineLocations[row][column] = true;
+                minesPlaced++;
+            }
+        }
+    }
+    private void cellRevealed() {
+        cellRevealed = new boolean[ROW_COUNT][COLUMN_COUNT];
+    }
 
 }
