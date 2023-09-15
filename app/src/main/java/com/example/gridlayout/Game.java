cc653eb11b933e1dbb21ed454618a5386fbc832e
Aaron Shields <amshield@usc.edu>
2023-09-14 19:41:56 -0700

    Inital Commit - Professor Helped w/ code + Game.java creation

---
+++ app/src/main/java/com/example/gridlayout/Game.java
@@ -0,0 +1,22 @@
+package com.example.gridlayout;
+
+
+public class Game {
+
+    public static final int ROW_COUNT = 12;
+    public static final int COLUMN_COUNT = 10;
+    public static final int MINE_COUNT = 4;
+
+    private boolean gameWon;
+    private boolean gameLost;
+
+    // Define your game logic here
+    // You can include methods and fields related to your game
+
+    public Game() {
+        // Initialize your game state here
+    }
+
+    // Add methods and fields specific to your game
+
+}
